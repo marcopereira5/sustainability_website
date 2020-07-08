d68dc5a65efe60045b46ffc118387810fbd28443
marcopereira5 <64165840+marcopereira5@users.noreply.github.com>
2020-07-08 15:19:36 +0100

    2 Commit
    Some Tweaking

--- www/scripts/landing_script.js
+++ www/scripts/landing_script.js
@@ -10,3 +10,28 @@ const t1 = new TimelineMax();
 t1.fromTo(mainPage, 1, {width: "0%"}, {width: "100%"}).fromTo(introduction, 1, {height: "200%"}, {height: "20%"})
 .fromTo(menu, 1.5, {opacity: "0"}, {opacity: "1"}).fromTo(toggle, 1, {opacity: "0"}, {opacity: "1"}, "-=1.5");
 
+function showDefinition(){
+
+    var definition = document.getElementsByClassName('definition')[0];
+    definition.style.display = 'block';
+
+    t1.fromTo(definition, 1, {opacity: "0"}, {height: "1"});
+
+    introduction.style.display = 'none';
+
+    mainPage.className = '';
+}
+
+function showMainPage(){
+
+    var definition = document.getElementsByClassName('definition')[0];
+    definition.style.display = 'none';
+
+    introduction.style.display = 'block';
+
+    mainPage.className = 'mainpage';
+
+    t1.fromTo(mainPage, 1, {width: "0%"}, {width: "100%"}).fromTo(introduction, 1, {height: "200%"}, {height: "20%"})
+.fromTo(menu, 1.5, {opacity: "0"}, {opacity: "1"}).fromTo(toggle, 1, {opacity: "0"}, {opacity: "1"}, "-=1.5");
+}
+
